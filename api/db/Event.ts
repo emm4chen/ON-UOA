704fd107338346961eb0295ad0c4fbc351e37ef6
emm4chen <117613372+emm4chen@users.noreply.github.com>
2024-04-09 18:00:08 +1200

    merged

---
+++ api/db/Event.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, model } from 'mongoose';
+
+const eventSchema = new Schema({
+    club: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    eventType: {
+        type: String,
+        enum : ['social','tech', 'cultural'],
+        default: 'social'
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 200,
+    }
+     
+});
